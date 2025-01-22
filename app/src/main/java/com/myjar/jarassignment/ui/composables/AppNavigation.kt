592c91fa61641aa50a06d875953d6ca1916ebda6
Suraj Verma <surajverma0347@gmail.ocm>
2025-01-22 14:47:09 +0530

    Implement search for computer items

--- app/src/main/java/com/myjar/jarassignment/ui/composables/AppNavigation.kt
+++ app/src/main/java/com/myjar/jarassignment/ui/composables/AppNavigation.kt
@@ -1,5 +1,6 @@
 package com.myjar.jarassignment.ui.composables
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +11,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -57,7 +64,12 @@ fun ItemListScreen(
     navigate: MutableState<String>,
     navController: NavHostController
 ) {
-    val items = viewModel.listStringData.collectAsState()
+    val items by viewModel.listStringData.collectAsState()
+    var computerItems by remember { mutableStateOf(items) }
+
+    LaunchedEffect(items) {
+        computerItems = items
+    }
 
     if (navigate.value.isNotBlank()) {
         val currRoute = navController.currentDestination?.route.orEmpty()
@@ -65,17 +77,59 @@ fun ItemListScreen(
             navController.navigate("item_detail/${navigate.value}")
         }
     }
-    LazyColumn(
+
+    SearchItems(
+        computerItems,
+        onNavigateToDetail
+    )
+}
+
+@Composable
+fun SearchItems(
+    items: List<ComputerItem>,
+    onNavigateToDetail: (String) -> Unit
+) {
+    var searchQuery by remember { mutableStateOf("") }
+    var filteredItems by remember { mutableStateOf(items) }
+
+    LaunchedEffect(searchQuery) {
+        filteredItems = if (searchQuery.isEmpty()) {
+            items
+        } else {
+            items.filter { it.name.contains(searchQuery, ignoreCase = true) }
+        }
+        Log.d("SearchItems", filteredItems.toString())
+    }
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(8.dp)
     ) {
-        items(items.value) { item ->
-            ItemCard(
-                item = item,
-                onClick = { onNavigateToDetail(item.id) }
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp)
+                .padding(8.dp),
+            textStyle = TextStyle(color = Color.Black),
+            colors = TextFieldDefaults.colors(focusedTextColor = Color.Black)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(filteredItems) { item ->
+                ItemCard(
+                    item = item,
+                    onClick = { onNavigateToDetail(item.id) }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
         }
     }
 }
@@ -88,7 +142,7 @@ fun ItemCard(item: ComputerItem, onClick: () -> Unit) {
             .padding(8.dp)
             .clickable { onClick() }
     ) {
-        Text(text = item.name, fontWeight = FontWeight.Bold, color = Color.Transparent)
+        Text(text = item.name, fontWeight = FontWeight.Bold)
     }
 }
 
